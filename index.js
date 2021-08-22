9147bed088a113d8c45231dc9a3691350081489f
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2021-08-22 03:14:17 -0700

    swiggy-project

---
+++ index.js
@@ -0,0 +1,15 @@
+
+    window.onload = function () {
+        console.log("loaded")
+        var typed = new Typed('#typed', {
+            strings: ["Game night?", "Hungry?", "", "Unexpected guests?", "Late night at office?", "Cooking gone wrong?", "Movie marathon?"],
+            backSpeed: 25,
+            smartBackspace: true,
+            backDelay: 1200,
+            startDelay: 1000,
+            typeSpeed: 55,
+            loop: true,
+        });
+    };
+
+
